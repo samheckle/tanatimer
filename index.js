31aa8169e89ff7f56869206d26d9d4532b4adc44
Sam Heckle <samheckle@yahoo.com>
2020-08-24 18:20:24 -0700

    status updating

--- index.js
+++ index.js
@@ -6,50 +6,56 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-var timing;             // interval timer
-var minutes = 0;        // minutes for each interval eg. 25 or 5
-var timePassed = 0;     // how much time has passed in pomo so far
-var time;               // date object to parse for time
-var working = false;  // which interval we are on eg. working or break
+var timing; // interval timer
+var minutes = 0; // minutes for each interval eg. 25 or 5
+var timePassed = 0; // how much time has passed in pomo so far
+var time; // date object to parse for time
+var working = false; // which interval we are on eg. working or break
 
 function setPomo(msg) {
-    working = !working;
+  working = !working;
 
-    if (!working) {
-      // sets value for break time eg. 5 minutes
-      minutes = 5;
-      message = "ding! work time!";
-    } else {
-      // sets value for initial time eg. 25 minutes
-      minutes = 25;
-      message = "ding! break time!";
-    }
+  if (!working) {
+    // sets value for break time eg. 5 minutes
+    minutes = 5;
+    message = "ding! work time!";
+    client.user
+      .setPresence({ activity: { name: "break time!" } })
+  } else {
+    // sets value for initial time eg. 25 minutes
+    minutes = 25;
+    message = "ding! break time!";
+    client.user
+      .setPresence({ activity: { name: "work time!" } })
+  }
 
-    countingTime(minutes);
+  countingTime(minutes);
 
-    timer = setTimeout(() => {
-      msg.channel.send(message);
-      clearInterval(timing);
-      timePassed = 0;
-      setPomo(msg);
-    }, minutes * 60 * 1000);
-}
-
-function reset(){
+  timer = setTimeout(() => {
+    msg.channel.send(message);
+    clearInterval(timing);
     timePassed = 0;
-    timeRemaining = 0;
-    intervalCount = 0;
-    working = false;
-    clearTimeout(timer);
-    clearInterval(timing); 
+    setPomo(msg);
+  }, minutes * 60 * 1000);
 }
 
-function countingTime(min){
-    timing = setInterval(() => {
-        timePassed++;
-        timeRemaining = (min*60)-timePassed
-        time = new Date(timeRemaining * 1000).toISOString().substr(14,5);
-      }, 1000);
+function reset() {
+  timePassed = 0;
+  timeRemaining = 0;
+  intervalCount = 0;
+  working = false;
+  clearTimeout(timer);
+  clearInterval(timing);
+  client.user
+    .setPresence({ activity: { name: "anti-pomo time" } })
+}
+
+function countingTime(min) {
+  timing = setInterval(() => {
+    timePassed++;
+    timeRemaining = min * 60 - timePassed;
+    time = new Date(timeRemaining * 1000).toISOString().substr(14, 5);
+  }, 1000);
 }
 
 client.on("message", (msg) => {
@@ -65,6 +71,8 @@ client.on("message", (msg) => {
   // starting pomo
   if (command === "!start") {
     msg.channel.send("starting work day!");
+    client.user
+      .setPresence({ activity: { name: "work time!" } })
     intervalCount = 0;
     setPomo(msg);
   } else if (command === "!stop") {
@@ -72,18 +80,24 @@ client.on("message", (msg) => {
     msg.channel.send("stopping work time!");
   } else if (command === "!time") {
     msg.channel.send(
-      "Time remaining " + time.substr(0,2) + " min " + time.substr(3,2) + " sec"
+      "Time remaining " +
+        time.substr(0, 2) +
+        " min " +
+        time.substr(3, 2) +
+        " sec"
     );
-  } else if (command === "!lunch"){
-      reset()
-      msg.channel.send("lunch time!");
-      let lunchMinutes = 30;
-      countingTime(lunchMinutes);
-      setTimeout(function(){
-        reset();
-        msg.channel.send("work time!");
-        setPomo(msg);
-      }, lunchMinutes*20*1000)
+  } else if (command === "!lunch") {
+    reset();
+    msg.channel.send("lunch time!");
+    let lunchMinutes = 30;
+    countingTime(lunchMinutes);
+    client.user
+      .setPresence({ activity: { name: "lunch time!" } })
+    setTimeout(function () {
+      reset();
+      msg.channel.send("work time!");
+      setPomo(msg);
+    }, lunchMinutes * 20 * 1000);
   }
 });
 
